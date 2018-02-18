44335ff4829bff2b3a2eacd0e145b1d779492323
denfed <dcfedori@buffalo.edu>
2018-02-18 09:05:15 -0500

    Some stuff

--- Utilities/kerascnn.py
+++ Utilities/kerascnn.py
@@ -4,11 +4,11 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
-from sklearn import preprocessing
+from keras.models import model_from_json
 
 batch_size = 25
 epochs = 5
-n = 4000
+n = 1000
 en = n - 50
 
 K.set_image_dim_ordering('tf')
@@ -49,9 +49,33 @@ model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimi
 model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, y_test))
 
 score = model.evaluate(X_test, y_test, verbose=0)
+test = model.predict(X_test)
+
 
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+
+#serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to hdf5
+model.save_weights("model.h5")
+print ("Saved model to disk")
+
+#load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+#evaluate loaded model on test data
+values = loaded_model.predict(X_test)
+print (values)
+
 
 
 
